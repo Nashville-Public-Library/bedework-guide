9ad066da9235a0638e75a64f66cb4fdcbf46fb0b
Kyle Cook <kyle.cook@nashville.gov>
2022-05-10 09:14:56 -0500

    updating link to bedework documentation from docusaurus tutorial 5 min

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Bedework Guide
           </Link>
         </div>
       </div>
@@ -29,8 +29,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title} | Home`}
+      description="Documentation for using Bedework Enterprise Calendar software at Nashville Public Library">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
